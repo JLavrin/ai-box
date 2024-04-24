216559fd0f84950c814bc256dee96d7f378c455c
jlavrin <julian.lavrin@icloud.com>
2024-04-24 23:02:07 +0200

    feat: fetch data

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,9 @@
+import apiClient from '../shared/client'
+
 
 export default async function Page() {
-
+  const response = await apiClient.get('/posts')
+  console.log(response.data)
   return (
     <div>
       {[].map((post) => (
