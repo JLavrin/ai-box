4f49f0c2a17828a0ab2a6b8d1e9489f6bf47cfc8
jlavrin <julian.lavrin@icloud.com>
2024-05-04 03:51:28 +0200

    feat: workspace adjust

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import { ConfigService } from './common/config/config.service'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
