5733820240c9788d882c658e80b302a2b6e093f2
jlavrin <julian.lavrin@icloud.com>
2024-05-04 03:54:47 +0200

    feat: workspace adjust

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { ConfigService } from './common/config/config.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
-  const config = app.use(ConfigService)
+  const config = app.get(ConfigService)
   await app.listen(config.get('PORT'))
 }
 bootstrap()
